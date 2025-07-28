d0e38df6e78a20bb4960a6c29cec843695c90b38
gimeng11 <2071199@hansung.ac.kr>
2025-07-28 17:04:54 +0900

    index test

--- backend/src/main/java/com/delicious/backend/config/WebConfig.java
+++ backend/src/main/java/com/delicious/backend/config/WebConfig.java
@@ -1,16 +1,20 @@
 package com.delicious.backend.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.file.Paths;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry
-                .addResourceHandler("/**")
-                .addResourceLocations("file:../frontend/index/"); // 실제 파일 위치
+        // 실제 경로: 프로젝트 루트 기준 ../frontend/
+        String frontendPath = Paths.get("../../frontend").toAbsolutePath().toUri().toString();
+
+        registry.addResourceHandler("/**")
+                .addResourceLocations("file:" + frontendPath);
     }
 }
-
