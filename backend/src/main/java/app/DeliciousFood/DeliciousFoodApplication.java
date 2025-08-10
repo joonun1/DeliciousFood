86ba2369bf76794d658c1a439a1960a29746beb6
gimeng11 <2071199@hansung.ac.kr>
2025-08-10 14:40:13 +0900

    make_backend

--- backend/src/main/java/app/DeliciousFood/DeliciousFoodApplication.java
+++ backend/src/main/java/app/DeliciousFood/DeliciousFoodApplication.java
@@ -1,13 +1,13 @@
-package com.delicious.backend;
+package app.DeliciousFood;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class DeliciousFoodApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(DeliciousFoodApplication.class, args);
 	}
 
 }
--- backend/src/test/java/app/DeliciousFood/DeliciousFoodApplicationTests.java
+++ backend/src/test/java/app/DeliciousFood/DeliciousFoodApplicationTests.java
@@ -1,10 +1,10 @@
-package com.delicious.backend;
+package app.DeliciousFood;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendApplicationTests {
+class DeliciousFoodApplicationTests {
 
 	@Test
 	void contextLoads() {
